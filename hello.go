23c0ded9a41eed678489956fbf74e89c0408d22a
George Bragin <gbragin@gmail.com>
2016-10-29 23:39:20 +0300

    strings.Join added

--- hello.go
+++ hello.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-       "fmt"
-       "os"
-)
+    "fmt"
+    "os"
+    "strings"
+    )
 
 func main() {
-     s, sep := "", ""
-     for _, arg := range os.Args[1:] {
-     	 s += sep + arg
-	 sep = " "
-     }
-     fmt.Printf(s + "\n")
+    fmt.Println(strings.Join(os.Args[1:], " "))
 }
